ce4683897c23094b5de25c65731426235412cd32
youngzheimer <youngzheimer@gmail.com>
2025-07-28 20:33:09 +0900

    docker

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# 현재 디렉토리를 Python path에 추가
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from utils import log, Config
 from file_watcher import FileWatcher
 
